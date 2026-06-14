e960ac8421dd96b551fbf87efd382cfcf7bf9a36
ishaandesai21 <ishaandesai21@gmail.com>
2026-06-14 14:26:00 -0500

    config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,6 @@ export const BASE_URL =
   "https://script.google.com/macros/s/AKfycbz5NqY6BH0l2xJGXTOa3aDHPB8CM3BxTr1jL1dGB7ZUB1fgcEBW1z_-QWlVMBRwJ4ILcQ/exec";
 
 export const GOOGLE_CLIENT_ID =
-  "240757796429-d88lrfdtlvtf6fgulhq1t3f28thre90k.apps.googleusercontent.com";
+  "705143016367-7vbcj2djsku8mt9nb3tf0stkhclkomac.apps.googleusercontent.com";
 
 export const FINANCE_DIRECTOR = "Ishaan";
